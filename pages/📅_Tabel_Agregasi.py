15d8cfac037933bcb63d8c1bbf8050f884036f51
markskjaer <143985997+markskjaer@users.noreply.github.com>
2023-10-19 14:02:20 +0700

    Update 📅_Tabel_Agregasi.py
    Signed-off-by: markskjaer <143985997+markskjaer@users.noreply.github.com>

--- pages/📅_Tabel_Agregasi.py
+++ pages/📅_Tabel_Agregasi.py
@@ -9,6 +9,7 @@ dataumkm = conn.read(spreadsheet=url)
 type(dataumkm)
 dataumkm["NIK"] = dataumkm["NIK"].astype("string")
 dataumkm["No HP"] = dataumkm["No HP"].astype("string")
+dataumkm['ALAMAT'] = dataumkm['ALAMAT'].str.rstrip()
 dataumkm['RW'] = dataumkm['ALAMAT'].str[-5:]
 dataumkm['RT'] = dataumkm['ALAMAT'].str[-11:]
 df = dataumkm
@@ -51,20 +52,27 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
             left, right = st.columns((1,20))
             left.write("↳")
             #Treat columns with < 10 unique values a s categorical
-            if is_categorical_dtype(df[column]):
+            if is_numeric_dtype(df[column]):
+                st.write(f'Values for {column}')
+                _min = st.text_input('Rentang Terkecil', float(df[column].min()))
+                _max = st.text_input('Rentang Tertinggi', float(df[column].max()))
+                _min = float(_min)
+                _max = float(_max)
+                #user_num_input = right.slider(
+                 #f"Values for {column}",
+                 #min_value=_min,
+                 #max_value=_max,
+                 #value=(_min, _max),
+                 #step=step,
+            #)
+                df= df[df[column].between(_min,_max)]
+            else:
                 user_cat_input = right.multiselect(
                     f"Values for {column}",
                     df[column].unique(),
                     default=list(df[column].unique()),
                     )
                 df = df[df[column].isin(user_cat_input)]
-            elif is_numeric_dtype(df[column]):
-                st.write(f'Values for {column}')
-                _min = st.text_input('Rentang Terkecil', float(df[column].min()))
-                _max = st.text_input('Rentang Tertinggi', float(df[column].max()))
-                _min = float(_min)
-                _max = float(_max)
-                df= df[df[column].between(_min,_max)]
         return df
 filtered_df = filter_dataframe(df)
 df = pd.DataFrame(filtered_df)
@@ -72,7 +80,7 @@ df = pd.DataFrame(filtered_df)
 a = st.sidebar.radio('Jumlah Karakteristik yang Ingin Ditampilkan:', [1, 2])
 
 if a == 1:
-    kolom_1 = st.selectbox("Pilih Karakteristik", dataumkm.columns, index=7)
+    kolom_1 = st.selectbox("Pilih Karakteristik", df.columns, index=7)
     pivot = df.pivot_table(index = [kolom_1],
                              values = ['NAMA PEMILIK'],
                              aggfunc = "count"
@@ -80,8 +88,8 @@ if a == 1:
     pivot.rename(columns = {'NAMA PEMILIK':'JUMLAH'}, inplace = True)
     st.write(pivot)
 elif a== 2:
-    kolom_1 = st.selectbox("Pilih Karakteristik Pertama", dataumkm.columns, index=7)
-    kolom_2 = st.selectbox("Pilih Karakteristik Kedua", dataumkm.columns, index=17)
+    kolom_1 = st.selectbox("Pilih Karakteristik Pertama", df.columns, index=7)
+    kolom_2 = st.selectbox("Pilih Karakteristik Kedua", df.columns, index=17)
     pivot = df.pivot_table(index = [kolom_1,kolom_2],
                              values = ['NAMA PEMILIK'],
                              aggfunc = "count"

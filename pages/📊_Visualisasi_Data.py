d66824b50b7f17851d402c3451ac1b4286e1ff11
markskjaer <143985997+markskjaer@users.noreply.github.com>
2023-10-20 14:49:49 +0700

    Update 📊_Visualisasi_Data.py
    Signed-off-by: markskjaer <143985997+markskjaer@users.noreply.github.com>

--- pages/📊_Visualisasi_Data.py
+++ pages/📊_Visualisasi_Data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
+import plotly.express as px
 
 ### Import Data Lengkap
 url = 'https://docs.google.com/spreadsheets/d/1qKf_0z1CcBfFxCejbReILQHVLCV0znxQZc2ob2qjPGg/edit?usp=sharing'
@@ -70,18 +71,37 @@ df = pd.DataFrame(filtered_df)
 
 st.markdown("# Grafik")
 
-#Dropdown Menu
-kolom_1 = st.selectbox("Pilih Karakteristik Pertama", df.columns, index=7)
-kolom_2 = st.selectbox("Pilih Karakteristik Kedua", df.columns, index=17)
+a = st.sidebar.radio('Jumlah Karakteristik yang Ingin Divisualisasikan:', [1, 2])
 
-###Bar Chart
-st.subheader("Grafik Batang")
-tes = pd.crosstab(df[kolom_1],df[kolom_2])
-st.bar_chart(data = tes, height = 700)
+if a==1:
+    #Dropdown Menu
+    kolom_1 = st.selectbox("Pilih Karakteristik", dataumkm.columns, index=7)
 
-st.subheader("Scatter Plot")
-st.write("Grafik ini menunjukkan pelaku UMKM di Desa Sekarwangi menurut besarnya modal usaha dan pendapatan usaha per bulan.")
-import plotly.express as px
-fig = px.scatter(
-    data_frame=df, x="BESARNYA MODAL USAHA", y="PENDAPATAN PER BULAN",color=kolom_1,symbol=kolom_2)
-st.plotly_chart(fig)
+    ###Bar Chart
+    st.subheader("Grafik Batang")
+    tes = dataumkm.pivot_table(index=dataumkm[kolom_1],values='NIK',aggfunc='count')
+    st.bar_chart(data = tes, height = 700)
+
+    st.subheader("Scatter Plot")
+    st.write("Grafik ini menunjukkan pelaku UMKM di Desa Sekarwangi menurut besarnya modal usaha dan pendapatan usaha per bulan.")
+
+    fig = px.scatter(
+        data_frame=dataumkm, x="BESARNYA MODAL USAHA", y="PENDAPATAN PER BULAN",color=kolom_1,symbol=kolom_1)
+    st.plotly_chart(fig)
+
+elif a==2:
+    #Dropdown Menu
+    kolom_1 = st.selectbox("Pilih Karakteristik Pertama", dataumkm.columns, index=7)
+    kolom_2 = st.selectbox("Pilih Karakteristik Kedua", dataumkm.columns, index=17)
+
+    ###Bar Chart
+    st.subheader("Grafik Batang")
+    tes = pd.crosstab(dataumkm[kolom_1],dataumkm[kolom_2])
+    st.bar_chart(data = tes, height = 700)
+
+    st.subheader("Scatter Plot")
+    st.write("Grafik ini menunjukkan pelaku UMKM di Desa Sekarwangi menurut besarnya modal usaha dan pendapatan usaha per bulan.")
+
+    fig = px.scatter(
+        data_frame=dataumkm, x="BESARNYA MODAL USAHA", y="PENDAPATAN PER BULAN",color=kolom_1,symbol=kolom_2)
+    st.plotly_chart(fig)

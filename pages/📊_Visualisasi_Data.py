9673197cddcf0e01a8c5acdd1fcdfddef8caa0e6
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-09 11:17:35 +0700

    Update 📊_Visualisasi_Data.py
    Signed-off-by: markskjaer <143985997+markskjaer@users.noreply.github.com>

--- pages/📊_Visualisasi_Data.py
+++ pages/📊_Visualisasi_Data.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 ### Import Data Lengkap
 url = 'https://docs.google.com/spreadsheets/d/1qKf_0z1CcBfFxCejbReILQHVLCV0znxQZc2ob2qjPGg/edit?usp=sharing'
-conn  = st.experimental_connection("gsheets", type=GSheetsConnection)
+conn  = st.connection("gsheets", type=GSheetsConnection)
 dataumkm = conn.read(spreadsheet=url)
 type(dataumkm)
 dataumkm["NIK"] = dataumkm["NIK"].astype("string")
